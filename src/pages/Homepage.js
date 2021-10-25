b25c2b873984dab9ee2d4a1ad9cbdc19d397a2b2
pj <Apple@Apples-MacBook-Pro.local>
2021-10-25 17:52:06 +0530

    added gallery faq and footer in homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -6,20 +6,40 @@ import Card from "../components/Card"
 const Home = () => {
     return ( 
         <div className="main w-full items-center justify-center">
-            <div className="hero p-8 flex items-center justify-center">
-            <div className="w-1/2">
+            <div className="section px-8 items-center justify-center">
+            <div className="p-6">
             <p className="">
-                <p className='text-3xl'>Welcome to Travdays</p>
+                <p className='text-5xl'>Welcome to Travdays</p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
                 Nostrum voluptatem ut sed tempora rerum vitae, 
                 atque dolores neque est quae tempore veritatis alias nam inventore esse illum illo et dolorem.
             </p>
+                <button className='mx-auto p-2 w-40 bg-blue-500'>Contact us</button>
             </div>
-            <div className='grid grid-cols-3 w-1/2'>
+            <div className='grid grid-cols-3'>
                 <Card/>
                 <Card/>
                 <Card/>
             </div>
+            </div>
+            <div className="section px-8 items-center justify-center">
+                <div className="gallery-preview">   
+                </div>
+                <div className="p-6">
+            <p className="">
+                <p className='text-5xl'>Gallery</p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Nostrum voluptatem ut sed tempora rerum vitae, 
+                atque dolores neque est quae tempore veritatis alias nam inventore esse illum illo et dolorem.
+            </p>
+                
+            </div>
+            </div>
+            <div className="section">
+
+            </div>
+            <div className="footer h-32 bg-[#FFA914]">
+
             </div>
             <Router>
                 <FAQ/>
