979d868def5f9f447649bb151ca77ade5b65dfd8
pj <Apple@Apples-MacBook-Pro.local>
2021-11-05 17:09:13 +0530

    set hieght of menu to 100vh, added gradient to package cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,9 @@
 
 const Card = ({title}) => {
     return ( 
-      <div className="card">
-          <p className='pl-4'>{title}</p>
+      <div className="card relative">
+          <div className='flex w-full h-1/2 bg-gradient-to-t from-[#00000088] to-[#00000000] absolute bottom-0 z-[0]'></div>
+          <p className='relative z-[1] pl-4'>{title}</p>
       </div>
      );
 }
