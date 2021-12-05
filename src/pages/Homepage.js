ffc40641d51c3beba91bf58c392655b3336da8c8
unknown <paramjoshi27@gmail.com>
2021-12-06 00:15:39 +0530

    waves added

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -62,7 +62,7 @@ const Home = ({isadmin,setIsadmin}) => {
                 <p className='text-3xl sm:text-2xl sm:leading-tight font-semibold leading-[0]'>Welcome to</p>
                 <p className="w-full text-8xl sm:text-6xl font-bold">Travdays</p>
             </div>
-            <div className='card-grid sm:py-[20px] w-[800px] mx-auto'  >
+            <div className='card-grid sm:py-[20px]  max-w-[800px]'  >
                 <Link className='card-grid__link__hover' to='/trips/workation'><Card title="Workation" id='one'  /></Link>
                 <Link className='card-grid__link__hover' to='/trips/solo'><Card title="Solo Travel"id='two' /></Link>
                 <Link className='card-grid__link__hover' to='/trips/pet friendly'><Card title="Pet Friendly" id='three' /></Link>
@@ -100,7 +100,9 @@ const Home = ({isadmin,setIsadmin}) => {
                 <div class="swiper-pagination"></div>
                 </div> */}
             </div>
-            <div className="section gallery grid grid-cols-2 justify-center">
+            <div className="section gallery grid grid-cols-2 justify-center relative">
+            <svg className='absolute top-[-426px] w-[1920px]' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#046C6D" fill-opacity="1" d="M0,288L80,277.3C160,267,320,245,480,240C640,235,800,245,960,245.3C1120,245,1280,235,1360,229.3L1440,224L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+            {/* <svg className='absolute top-[-246px] w-[1920px]' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#000088" fill-opacity="1" d="M0,192L80,176C160,160,320,128,480,144C640,160,800,224,960,261.3C1120,299,1280,309,1360,314.7L1440,320L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg> */}
                 <div className="flex items-center justify-center">
                 <div className="gallery-preview">   
                 </div>
@@ -117,7 +119,10 @@ const Home = ({isadmin,setIsadmin}) => {
             </div>
             </div>
             <div>
-            <div className="section">
+            <div className="section testimonial relative">
+            {/* <svg className='absolute top-[-426px] w-[1920px]' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#F7F7F5" fill-opacity="1" d="M0,224L80,240C160,256,320,288,480,266.7C640,245,800,171,960,144C1120,117,1280,139,1360,149.3L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg> */}
+            <svg className='absolute top-[-360px] left-0 w-[1920px]'  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,256L60,256C120,256,240,256,360,261.3C480,267,600,277,720,266.7C840,256,960,224,1080,224C1200,224,1320,256,1380,272L1440,288L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
+            {/* <svg className='absolute top-[-426px] w-[1920px]' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,256L60,245.3C120,235,240,213,360,197.3C480,181,600,171,720,160C840,149,960,139,1080,144C1200,149,1320,171,1380,181.3L1440,192L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg> */}
             <span className='text-6xl sm:text-4xl font-bold p-6 sm:p-2 inline-block'>Testimonials</span>
             <div className="grid grid-cols-2 ">
                     <div className='flex h-32 '>
