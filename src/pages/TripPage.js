00ee24b13f82226e6074780634b10bd40455466c
Kanishk666 <kkaranpuria@gmail.com>
2021-12-08 19:27:05 +0530

    booking&tripPage working AGAIN

--- src/pages/TripPage.js
+++ src/pages/TripPage.js
@@ -6,29 +6,32 @@ import { useState, useRef, useCallback } from "react";
 import { useEffect } from "react";
 
 
-const Trip = () => {
+const Trip = ({ isAuth }) => {
 
     const { name } = useParams()
 
     let history = useHistory()
 
     const [page, setPage] = useState(1) // page 1 pe req initial run fir ++ hota jaayega
-    const [loading, setLoading] = useState(true) // initially true 
+    const [loading, setLoading] = useState(false) // initially false
+    const [loadingdone2, setLoadingdone2] = useState(false) // initially false 
+    const [loadingdone3, setLoadingdone3] = useState(false) // initially false 
     const [hasMore, setHasMore] = useState(true)
 
     const [backToDisplay, setBackToDisplay] = useState(false) // to come back from book now feature
 
     const [infoObject, setInfoObject] = useState([])
-    const [mediaObject, setMediaObject] = useState([])
+    const [mediaObject, setMediaObject] = useState(null)
     const [reviewObject, setReviewObject] = useState([])
     const [reviewCreationBool, setReviewCreationBool] = useState(false)
-
+    const [refetch, setRefetch] = useState(true)
     const [isbooking, setIsbooking] = useState(false)
-    const [isAuth, setIsAuth] = useState(null)
 
     const [bookingQuery, setBookingQuery] = useState('')
     const [phoneNumber, setPhoneNumber] = useState(0)
     const [errorForEmptySubmission, setErrorForEmptySubmission] = useState(false)
+
+    const [reviewSubmitted, setReviewSubmitted] = useState(false)
 
     const observer = useRef('') // has only one attribute - current!
 
@@ -38,27 +41,34 @@ const Trip = () => {
     useEffect(() => {
         setIsbooking(false);
         setBackToDisplay(false);
+        setLoadingdone2(false)
+        setLoadingdone3(false)
         fullaxios({ url: 'trip/' + name, sendcookie: false })
             .then(res => {
-                // console.log('trip info : \n ' + res.data)
-                // console.log('info data received')
-                setInfoObject(res.data)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-
-        fullaxios({ url: 'userinfo/status' })
-            .then(res => {
                 if (res) {
-                    // console.log(res.data)
-                    setIsAuth(res.data.authenticated)
-                    console.log(isAuth)
+                    // console.log('trip info : \n ' + res.data)
+                    // console.log('info data received')
+                    setInfoObject(res.data)
+                    setLoadingdone2(true)
+
+
                 }
             })
             .catch(err => {
                 console.log(err)
             })
+
+        // fullaxios({ url: 'userinfo/status' })
+        //     .then(res => {
+        //         if (res) {
+        //             // console.log(res.data)
+        //             setIsAuth(res.data.authenticated)
+        //             console.log(isAuth)
+        //         }
+        //     })
+        //     .catch(err => {
+        //         console.log(err)
+        //     })
 
         fullaxios({ url: 'trip/media/' + name, sendcookie: false })
             .then(res => {
@@ -66,23 +76,50 @@ const Trip = () => {
                     console.log(res.data)
                     setMediaObject(res.data)
                     console.log(res.data)
+                    setLoadingdone3(true)
                 }
             })
             .catch(err => {
                 console.log(err)
             })
 
+    }, [backToDisplay || reviewSubmitted])
 
-        fullaxios({ url: 'trip/review/create/' + name, sendcookie: false })
+    useEffect(() => {
+        if (isAuth) {
+            fullaxios({ url: 'trip/review/create/' + name, sendcookie: false })
+                .then(res => {
+                    if (res) {
+                        console.log('review worth :\n' + res.data.bool)
+                        setReviewCreationBool(res.data.bool)
+                    }
+                })
+        }
+        else {
+            setReviewCreationBool(false)
+        }
+    }, [isAuth, backToDisplay])
+
+    const submitBooking = (e) => {
+        e.preventDefault()
+        fullaxios({
+            url: 'booking/', type: 'post', data: {
+
+                phone: phoneNumber,
+                query: bookingQuery,
+                trip: name,
+
+            }
+        })
             .then(res => {
-                if (res) {
-                    // console.log('review worth :\n' + res.data)
-                    setReviewCreationBool(res.data.bool)
-                }
+                console.log(res)
+                alert('ho gaya')
+
             })
-
-    }, [backToDisplay])
-
+            .catch(err => {
+                console.log(err)
+            })
+    }
 
     const lastDataElementRef = useCallback(node => {
         console.log('last element')
@@ -111,47 +148,56 @@ const Trip = () => {
             })
             .catch(err => {
                 if (err && (err.response.data.detail === "Invalid page.")) {
+                    // setLoading(true)
                     setHasMore(false)
                 }
             })
 
 
-    }, [page, backToDisplay])
+    }, [page, backToDisplay, refetch])
 
 
     const booking = () => {
-
-        setMediaObject(null);
-        setInfoObject(null);
-        setReviewObject(null);
-        setReviewCreationBool(false);
-        setIsbooking(true);
+        if (isAuth) {
+            setMediaObject(null);
+            setInfoObject(null);
+            setReviewObject([]);
+            setReviewCreationBool(false);
+            setPage(1);
+            setIsbooking(true);
+        }
+        else {
+            alert("You have to sign in to book the website")
+        }
     }
 
 
     const submitReview = () => {
-        if (userGivenDescription && userGivenStars){
-        fullaxios({
-            url: 'trip/review/create/' + name, type: 'post', data: {
-                ratings: userGivenStars,
-                description: userGivenDescription,
-                name: (infoObject.name)
-            }
-        })
-            .then(res => {
-                console.log('review submitted')
-                history.push(`/trip/${name}`)
-                setReviewObject([])
-                setHasMore(true)
-                setErrorForEmptySubmission(false)
-                setUserGivenDescription("")
-                setPage(1)
+        console.log("this is running for no reason at all")
+        if (userGivenDescription && userGivenStars) {
+            fullaxios({
+                url: 'trip/review/create/' + name, type: 'post', data: {
+                    ratings: userGivenStars,
+                    description: userGivenDescription,
+                    name: (infoObject.name)
+                }
             })
-            .catch(err => {
-                console.log(err)
-            })
+                .then(res => {
+                    console.log(res.data)
+                    console.log('review submitted')
+                    history.push(`/trip/${name}`)
+                    setReviewObject([])
+                    setHasMore(true)
+                    setErrorForEmptySubmission(false)
+                    setUserGivenDescription("")
+                    if (page === 1) { if (refetch === true) { setRefetch(false) } else { setRefetch(true) } }
+                    else { setPage(1) }
+                })
+                .catch(err => {
+                    console.log(err)
+                })
         }
-        else{
+        else {
             setErrorForEmptySubmission(true)
         }
     }
@@ -162,9 +208,14 @@ const Trip = () => {
 
     }, [isbooking, backToDisplay])
 
-    return (
-        <div className='w-[80%] mt-4 h-[90vh] overflow-hidden sm:max-w-full p-box-shadow-2 rounded-[20px] mb-4'>
-
+    return (<>
+        {/* {console.log("tf")} */}
+        {/* {console.log(loading)} */}
+        {console.log(loadingdone2)}
+        {console.log(loadingdone3)}
+        {loadingdone2 && loadingdone3 && <div className='w-[80%] mt-4 h-[90vh] overflow-hidden sm:max-w-full p-box-shadow-2 rounded-[20px] mb-4'>
+            {console.log("this should not work", infoObject)}
+            {console.log("hasmore", hasMore)}
             {!isbooking && <div className=" w-full tripPage ">
                 {/* <h2><button onClick={() => setLink(`explore`)}>All</button><button onClick={() => setLink(`explore/image`)}>Images</button><button onClick={() => setLink(`explore/audio`)}>Audio</button><button onClick={() => setLink(`explore/video`)}>Video</button></h2> */}
                 <div className='grid grid-cols-1 overflow-y-auto sm:rounded-none rounded-b-[20px] h-[90vh] sm:h-[85vh]'>
@@ -229,6 +280,28 @@ const Trip = () => {
                     <div className='px-2 divide-y divide-gray-500'>
 
                         <span className='flex text-3xl mb-6'>Reviews</span>
+
+                        {
+
+                            reviewCreationBool &&
+                            <div className="flex flex-col pt-5 pb-2">
+                                <h3>Enter Review Here:</h3>
+                                {/* <div className="flex">
+                <button className='p-2 bg-blue-500 font-semibold rounded-lg sm:mx-auto'onClick={() => { setUserGivenStars(1) }}>1</button>
+                <button className='p-2 bg-blue-500 font-semibold rounded-lg sm:mx-auto'onClick={() => { setUserGivenStars(2) }}>2</button>
+                <button className='p-2 bg-blue-500 font-semibold rounded-lg sm:mx-auto'onClick={() => { setUserGivenStars(3) }}>3</button>
+                <button className='p-2 bg-blue-500 font-semibold rounded-lg sm:mx-auto'onClick={() => { setUserGivenStars(4) }}>4</button>
+                <button className='p-2 bg-blue-500 font-semibold rounded-lg sm:mx-auto'onClick={() => { setUserGivenStars(5) }}>5</button>
+                </div> */}
+                                <div className="flex w-full">
+                                    <input className="w-1/2" required placeholder='Reviews..' value={userGivenDescription} onChange={(e) => setUserGivenDescription(e.target.value)} />
+                                    {errorForEmptySubmission && <p> Enter the review and Choose a rating to submit </p>}
+                                </div>
+                                <button className='p-2 w-40 bg-blue-500 font-semibold rounded-lg sm:mx-auto' onClick={() => { submitReview() }}>Submit Review</button>
+
+                            </div>
+                        }
+
                         {
                             reviewObject && reviewObject.map((data, index) => {
                                 if (reviewObject.length === index + 1) {
@@ -293,32 +366,10 @@ const Trip = () => {
                                 )
                             })
                         }
+
                     </div>
-                        {console.log("asdfdsf",reviewCreationBool)}
-                    {
-
-                        reviewCreationBool &&
-                        <div className="flex flex-col pt-5 pb-2">
-                            <h3>Enter Review Here:</h3>
-                            {/* <div className="flex">
-                <button className='p-2 bg-blue-500 font-semibold rounded-lg sm:mx-auto'onClick={() => { setUserGivenStars(1) }}>1</button>
-                <button className='p-2 bg-blue-500 font-semibold rounded-lg sm:mx-auto'onClick={() => { setUserGivenStars(2) }}>2</button>
-                <button className='p-2 bg-blue-500 font-semibold rounded-lg sm:mx-auto'onClick={() => { setUserGivenStars(3) }}>3</button>
-                <button className='p-2 bg-blue-500 font-semibold rounded-lg sm:mx-auto'onClick={() => { setUserGivenStars(4) }}>4</button>
-                <button className='p-2 bg-blue-500 font-semibold rounded-lg sm:mx-auto'onClick={() => { setUserGivenStars(5) }}>5</button>
-                </div> */}
-                            <div className = "flex w-full">
-                            <input className = "w-1/2" require placeholder='Reviews..' value = {userGivenDescription} onChange={(e) => setUserGivenDescription(e.target.value)} />
-                            {errorForEmptySubmission && <p> Enter the review and Choose a rating to submit </p>}
-                            </div>
-                            <button className='p-2 w-40 bg-blue-500 font-semibold rounded-lg sm:mx-auto' onClick={() => { submitReview() }}>Submit Review</button>
-                            
-                        </div>
-                    }
 
 
-
-                   
                     {
                         (isAuth === false) && isbooking &&
                         <div>
@@ -328,115 +379,30 @@ const Trip = () => {
                 </div>
             </div>}
             {
-                        isAuth && isbooking && <div className="flex flex-col justify-center items-center">
-                            <form className="flex flex-col" onSubmit={console.log('submit')}>
-                                <label className="flex items-center justify-center">
-                                    Enter your query:
-                                    <input placeholder='enter your query' onChange={(e) => { setBookingQuery(e.target.value) }} />
-                                </label>
-                                <label className="flex items-center">
-                                    Your Phone number:
-                                    <input placeholder='enter your phone number' required onChange={(e) => { setPhoneNumber(e.target.value) }} /><br />
-                                </label>
-                                <label className="flex items-center">
-                                    Your Trip:
-                                    <input readOnly value={name} />
-                                </label>
-                                <div className="flex">
-                                    <button className='p-2 w-40 bg-blue-500 font-semibold rounded-lg sm:mx-auto' onClick={() => { setBackToDisplay(true); console.log("wtaf") }} >Back</button>
-                                    <button className='p-2 w-40 bg-blue-500 font-semibold rounded-lg sm:mx-auto' type="submit">Submit</button>
-                                </div>
-                            </form>
+                isAuth && isbooking && <div className="flex flex-col justify-center items-center">
+                    <form className="flex flex-col" onSubmit={submitBooking}>
+                        <label className="flex items-center justify-center">
+                            Enter your query:
+                            <input placeholder='enter your query, if any' onChange={(e) => { setBookingQuery(e.target.value) }} />
+                        </label>
+                        <label className="flex items-center">
+                            Your Phone number:
+                            <input required type="number" placeholder='enter your phone number' requiredd onChange={(e) => { setPhoneNumber(e.target.value) }} /><br />
+                        </label>
+                        <label className="flex items-center">
+                            Your Trip:
+                            <input required readOnly value={name} />
+                        </label>
+                        <div className="flex">
+                            <button className='p-2 w-40 bg-blue-500 font-semibold rounded-lg sm:mx-auto' onClick={() => { setBackToDisplay(true); console.log("wtaf") }} >Back</button>
+                            <button className='p-2 w-40 bg-blue-500 font-semibold rounded-lg sm:mx-auto' type="submit">Submit</button>
+                        </div>
+                    </form>
 
-                        </div>}
-        </div>
-        // </div>
-        // <div className="review">
+                </div>}
+        </div>}
 
-        // {infoObject &&
-        //     <div>
-        //         {infoObject.name} <br />
-        //         {infoObject.type}<br />
-        //         {infoObject.description}<br />
-        //         {infoObject.price}<br />
-        //         {infoObject.ratings}<br />
-        //         {infoObject.ratingsCount}<br />
-
-        //     </div>}
-
-        // {
-        //     mediaObject && mediaObject.map((data) => {
-        //         return (
-        //             <div >
-        //                 {data.image && <img src={data.image} />}
-        //                 {data.video && <video controls src={data.video} alt='' width='100%' />}
-        //             </div>
-        //         )
-
-        //     }
-        //     )
-        // }
-
-        // {
-        //     reviewObject && reviewObject.map((data, index) => {
-        //         if (reviewObject.length === index + 1) {
-        //             return (<div ref={lastDataElementRef}>
-        //                 {data.user}
-        //                 <h2>description : {data.description}</h2>
-
-        //                 <h2>rating:{data.ratings}</h2>
-
-
-        //             </div>)
-        //         }
-
-        //         else return (
-        //             <div>
-        //                 {data.user} , {data.created}
-        //                 <h2>description : {data.description}</h2>
-
-        //                 <h2>rating:{data.ratings}</h2>
-        //             </div>)
-        //     })
-        // }
-
-        // {
-
-        //     reviewCreationBool &&
-        //     <div>
-        //         <h3>enter review here:</h3>
-        //         <button onClick={() => { setUserGivenStars(1) }}>1</button><br />
-        //         <button onClick={() => { setUserGivenStars(2) }}>2</button><br />
-        //         <button onClick={() => { setUserGivenStars(3) }}>3</button><br />
-        //         <button onClick={() => { setUserGivenStars(4) }}>4</button><br />
-        //         <button onClick={() => { setUserGivenStars(5) }}>5</button><br />
-
-
-        //         <input require placeholder='reviews..' onChange={(e) => setUserGivenDescription(e.target.value)} />
-
-        //         {userGivenDescription && userGivenStars && <div>
-        //             <button onClick={() => { submitReview() }}>SUBMIT</button>
-        //         </div>}
-        //         <button onClick={() => { booking() }}> BOOK NOW </button>
-
-        //     </div>
-        // }
-        // <br />
-
-
-
-        // {
-        //     isAuth && isbooking && <div className="">
-        //         <button onClick={() => { setBackToDisplay(true); console.log("wtaf") }} >back to trip page</button>
-        //     </div>}
-        // {
-        //     (isAuth === false)
-        //     && <div>
-        //         {alert('you arent logged in, to make a booking, log in.')}
-        //     </div>
-        // }
-        // </div>
-
+    </>
     );
 }
 
