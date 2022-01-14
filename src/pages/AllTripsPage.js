f3e9bb5a09f258ba92f4d0de1e463ca109ca854a
param joshi <paramjoshi27@gmail.com>
2022-01-14 20:32:12 +0530

    find bugs

--- src/pages/AllTripsPage.js
+++ src/pages/AllTripsPage.js
@@ -366,7 +366,7 @@ const AllTrips = () => {
 
 
   return (<>
-    <div className='section relative flex flex-col items-center '>
+    <div className='section relative flex flex-col items-center'>
 
       {/* <svg xmlns="http://www.w3.org/2000/svg" className="z-[5] h-16 w-16 fixed bottom-16 right-16 md:right-4 " viewBox="0 0 20 20" fill="currentColor">
         <path fillRule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clipRule="evenodd" />
