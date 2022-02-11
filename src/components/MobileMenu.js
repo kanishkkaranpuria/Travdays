e35320e64f111266386e5159a6d9bcd51e8c137f
Aum <aumghag7077@gmail.com>
2022-02-11 23:25:55 +0530

    mobile view navbar fixed

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -4,6 +4,8 @@ import fullaxios from "./FullAxios";
 import Cookie from "../components/Cookie";
 import { useHistory } from "react-router";
 import { NavLink } from "react-router-dom";
+import Logoutmodal from "./Logoutmodal";
+
 
 import logo from '../pages/images/TravDays_logos/transparent_logo.png'
 const hideMenu = ()=>{
@@ -19,6 +21,8 @@ const MobileMenu = ({ namechanged ,isauthenticated, setIsadmin, setIsauthenticat
     const [profiledata, setProfiledata] = useState([])
     const [igotdata, setIgotdata] = useState(false);
     const [profile, setProfile] = useState(false);
+    const [isopen, setIsopen] = useState(false);
+
 
 
     useEffect(() => {
@@ -32,22 +36,23 @@ const MobileMenu = ({ namechanged ,isauthenticated, setIsadmin, setIsauthenticat
             })
     }, [isauthenticated,namechanged])
     
-    const Profile = () => {
-        setProfile(true)
+    const Profile = (statement) => {
+        setProfile(statement)
     }
    
 
-    const logout = () => {
+    const Logout = () => {
         console.log("logout")
         fullaxios({ url: 'auth/logout', sendcookie: true })
             .then(res => {
                 //("here",res.data)
                 setProfile(false)
                 setIsadmin(false)
-                alert("you have logged out")
                 setIgotdata(false)
                 setProfiledata([])
                 setIsauthenticated(false)
+                setIsopen(true)
+
                 // Cookie('setCookie',"accesstoken",0,-1)
                 Cookie('setCookie', "csrftoken", 0, -1)
                 Cookie('setCookie', "sessionid", 0, -1)
@@ -58,35 +63,36 @@ const MobileMenu = ({ namechanged ,isauthenticated, setIsadmin, setIsauthenticat
             })
     }
     return ( 
-        <div onClick={hideMenu} id="mobile-menu" className="flex flex-col text-white bg-black fixed top-0 z-[20] w-full h-full opacity-90">
+        <div  id="mobile-menu" className="flex flex-col text-white bg-black fixed top-0 z-[20] w-full h-full opacity-90">
         
             <nav className="flex flex-col w-full h-full items-center justify-center">
                {profile ? <> 
-                <NavLink className='p-4 text-xl' to='/changename'><h4>Change Name</h4></NavLink>
-                            <NavLink className='p-4 text-xl' to='/bookings'><h4>Bookings</h4></NavLink>
-                            <NavLink className='p-4 text-xl' to='/resetpassword'><h4>Reset password</h4></NavLink>
-                            <NavLink className='p-4 text-xl' to='/myblogs'><h4> My Blogs</h4></NavLink>
-                            <p className='p-4 text-xl' onClick={logout} ><h4>Logout</h4></p>
+                <NavLink  onClick={()=>{Profile(false)}} className='p-4 text-xl' to='/'><h4 className=""> Go back </h4></NavLink>
+                <NavLink onClick={hideMenu} className='p-4 text-xl' to='/changename'><h4>Change Name</h4></NavLink>
+                            <NavLink onClick={hideMenu} className='p-4 text-xl' to='/bookings'><h4>Bookings</h4></NavLink>
+                            <NavLink  onClick={hideMenu} className='p-4 text-xl' to='/resetpassword'><h4>Reset password</h4></NavLink>
+                            <NavLink onClick={hideMenu}  className='p-4 text-xl' to='/myblogs'><h4> My Blogs</h4></NavLink>
                </>:<>
-               <Link className='p-6 text-3xl' to='/'>TravDays</Link>
+               <Link onClick={hideMenu} className='p-6 text-3xl' to='/'>TravDays</Link>
                 {/* <Link className='p-4 text-xl' to='/IndivisualBlogPage'>indi-blog</Link> */}
-                <Link className='p-4 text-xl' to='/trips'>Packages</Link>
+                <Link  onClick={hideMenu} className='p-4 text-xl' to='/trips'>Packages</Link>
                 {/* <Link className='p-4 text-xl' to='/blogs'>Blogs</Link> */}
-                <Link className='p-4 text-xl' to='/gallery'>Gallery</Link>
-                <Link className='p-4 text-xl' to='/contactus'>Contact Us</Link>
-                <Link className='p-4 text-xl' to='/faq'>FAQ</Link>
-                <NavLink className='p-4 text-xl' to='/blogs'><h4>Blogs</h4></NavLink>
+                <Link  onClick={hideMenu} className='p-4 text-xl' to='/gallery'>Gallery</Link>
+                <Link  onClick={hideMenu} className='p-4 text-xl' to='/contactus'>Contact Us</Link>
+                <Link  onClick={hideMenu} className='p-4 text-xl' to='/faq'>FAQ</Link>
+                <NavLink  onClick={hideMenu} className='p-4 text-xl' to='/blogs'><h4>Blogs</h4></NavLink>
                 {/* <Link className='p-4 text-xl' to='/bookings'>Bookings</Link>
                 <Link className='p-4 text-xl' to='/resetpassword'>Reset password</Link>
-                <Link className='p-4 text-xl' to='/changename'>Change name </Link> */}
+            <Link className='p-4 text-xl' to='/changename'>Change name </Link> */}
                 
                 {isauthenticated && igotdata ? <>
-                        <p   onClick={Profile} className='p-4 text-xl'  >Profile</p>
+                        <p   onClick={()=>{Profile(true)}} className='p-4 text-xl'  >Profile</p>
+                        <p  onClick={()=>{hideMenu();Logout()}} className='p-4 text-xl' ><h4>Logout</h4></p>
                             
                    </> :
-                    <NavLink className='p-4 text-xl' to='/login' >Login</NavLink>}
+                    <NavLink onClick={hideMenu} className='p-4 text-xl' to='/login' >Login</NavLink>}
                </>} 
-
+               <Logoutmodal setIsopen={setIsopen} isopen={isopen} headingg="Proccess complete" p1="You have been logged out" p2=""/>
             </nav>
         </div>
      );
