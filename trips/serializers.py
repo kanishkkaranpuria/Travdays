da7fba884aa0e3ae467f47e4e393a0b4e081ad42
Aditya <adityaatb@gmail.com>
2022-08-24 03:08:53 +0530

    domain url now wont be needed to be changed manually in thumbnail code

--- trips/serializers.py
+++ trips/serializers.py
@@ -54,7 +54,7 @@ class SingleTripMediaDisplaySerializer(serializers.ModelSerializer):
         if obj.image == '' or obj.image == None:
             clip = VideoFileClip(request.build_absolute_uri(obj.video.url))
             clip.save_frame(f"media/admin media/images/thumbnail{obj.id}.jpg",t=0.00) 
-            img_url = f"http://127.0.0.1:8000/media/admin media/images/thumbnail{obj.id}.jpg"
+            img_url = request.build_absolute_uri(f"/media/admin media/images/thumbnail{obj.id}.jpg")
             name = urlparse(img_url).path.split('/')[-1]
             response = requests.get(img_url)
             if response.status_code == 200:
